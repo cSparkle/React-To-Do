7a038e992b9299897f7912f728c7fd2b2bce895a
Cortney Thomas <thomas.cortney@outlook.com>
2018-08-21 10:12:37 -0400

    Checkpoint 7 ReactToDo State and Props: Completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        { description: 'do the dishes', isCompleted: false },
+        { description: 'pack for trip', isCompleted: false },
+        { description: 'finish Module 1 of WDT', isCompleted: true },
+        { description: 'fill the cat food bowl', isCompleted: false },
+        { description: 'make a smoothie', isCompleted: true }
+      ]
+    };
+  }
+  
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          {this.state.todos.map( (todo, index) => 
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted }/>
+          )}
         </ul>
       </div>
     );
